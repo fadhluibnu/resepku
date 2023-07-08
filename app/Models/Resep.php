530446552b0b60c71f420f09f74a18e70d5c2f0f
Fadhlu Ibnu <f.ibnu.a1@gmail.com>
2023-07-08 22:32:31 +0700

    relationship

--- app/Models/Resep.php
+++ app/Models/Resep.php
@@ -18,4 +18,9 @@ class Resep extends Model
         'image',
         'like',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     // protected $casts = [
     //     'email_verified_at' => 'datetime',
     // ];
+
+    public function reseps()
+    {
+        return $this->hasMany(Resep::class, 'user_id');
+    }
 }
